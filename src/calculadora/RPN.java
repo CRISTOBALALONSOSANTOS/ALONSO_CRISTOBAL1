37c898fc98d7a775a0243ccbbcf1b90f33bdac5f
alons <alons@LAPTOP-268CDABO>
2020-05-16 14:08:19 +0200

    Commmit 1 (cambio nombre de variables comando y arriba)

--- src/calculadora/RPN.java
+++ src/calculadora/RPN.java
@@ -2,70 +2,70 @@ package calculadora;
 
 public class RPN {
 	public void pushPila(double nuevo_dato) {
-		NodoPila nuevo_nodo = new NodoPila(nuevo_dato, arriba);
-		arriba = nuevo_nodo;
+		NodoPila nuevo_nodo = new NodoPila(nuevo_dato, up);
+		up = nuevo_nodo;
 	}
 	public double popPila( ) {
-		double dato_arriba = arriba.dato;
-		arriba = arriba.abajo;
+		double dato_arriba = up.dato;
+		up = up.abajo;
 		return dato_arriba;
 	}
 	public RPN(String commando) {
-		arriba = null;
-		this.commando = commando;
+		up = null;
+		this.command = commando;
 	}
 	public double resultado( ) {
 		double a, b;
 		int j;
-		for(int i = 0; i < commando.length( ); i++) {
+		for(int i = 0; i < command.length( ); i++) {
 			// si es un digito
-			if(Character.isDigit(commando.charAt(i))) {
+			if(Character.isDigit(command.charAt(i))) {
 				double numero;
 				// obtener un string a partir del numero
 				String temp = "";
 				for(j = 0; (j < 100) && (Character.isDigit(
-						commando.charAt(i)) || (commando.charAt(i) == '.')); j++, i++) {
-					temp = temp + String.valueOf(commando.
+						command.charAt(i)) || (command.charAt(i) == '.')); j++, i++) {
+					temp = temp + String.valueOf(command.
 							charAt(i));
 				}
 				// convertir a double y añadir a la pila
 				numero = Double.parseDouble(temp);
 				pushPila(numero);
-			} else if(commando.charAt(i) == '+') {
+			} else if(command.charAt(i) == '+') {
 				b = popPila( );
 				a = popPila( );
 				pushPila(a + b);
-			} else if(commando.charAt(i) == '-') {
+			} else if(command.charAt(i) == '-') {
 				b = popPila( );
 				a = popPila( );
 				pushPila(a - b);
-			} else if(commando.charAt(i) == '*') {
+			} else if(command.charAt(i) == '*') {
 				b = popPila( );
 				a = popPila( );
 				pushPila(a * b);
-			} else if(commando.charAt(i) == '/') {
+			} else if(command.charAt(i) == '/') {
 				b = popPila( );
 				a = popPila( );
 				pushPila(a / b);
 			}
-			else if(commando.charAt(i) == '^') {
+			else if(command.charAt(i) == '^') {
 				b = popPila( );
 				a = popPila( );
 				pushPila(Math.pow(a, b));}
-			else if(commando.charAt(i) == '%') {
+			else if(command.charAt(i) == '%') {
 				b = popPila( );
 				a = popPila( );
 				pushPila(a%b);
-			} else if(commando.charAt(i) != ' ') {
+			} else if(command.charAt(i) != ' ') {
 				throw new IllegalArgumentException( );
 			}
 		}
 		double val = popPila( );
-		if(arriba != null) {
+		if(up != null) {
 			throw new IllegalArgumentException( );
 		}
 		return val;
 	}
-	private String commando;
-	private NodoPila arriba;
+	private String command;
+	private NodoPila up;
 }
